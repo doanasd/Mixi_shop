6068545dab2b5c13c38f35b636baedd246640157
doanasd <121279957+doanasd@users.noreply.github.com>
2026-04-07 23:22:27 +0700

    Update database connection settings in connect.php
    Change database server name to match Docker service and retrieve password from environment variable.

--- src/func/connect.php
+++ src/func/connect.php
@@ -1,7 +1,11 @@
 <?php
-    $servername = "localhost";
+    // 1. Đổi thành tên service database trong file docker-compose.yml
+    $servername = "db"; 
     $usname = "root";
-    $pass = "";
+    
+    // 2. Lấy mật khẩu linh hoạt từ biến môi trường của Docker
+    $pass = getenv('DB_PASSWORD'); 
+    
     $database = "mixi_shop";
 
     $conn = new mysqli($servername, $usname, $pass, $database);
