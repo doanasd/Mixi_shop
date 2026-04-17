b966d5303f4530f0fcf8211210109f7a1aa840a8
Phạm Thanh Doãn <121279957+doanasd@users.noreply.github.com>
2026-04-17 10:21:37 +0700

    Update header.php

--- src/page/header.php
+++ src/page/header.php
@@ -15,7 +15,6 @@ include dirname(__DIR__) . "/func/logger.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mixi Shop</title>
-// <link rel="shortcut icon" href="img/download.jfif" type="image/x-icon">
     <link rel="stylesheet" href="style.css"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
